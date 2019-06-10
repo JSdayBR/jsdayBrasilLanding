18b1382ce1ff9434a46d1684a314a364fa6d8645
Felipe de Albuquerque <felipealbuquerque81@gmail.com>
2019-06-10 13:55:35 -0700

    fixed menu

--- js/demo.js
+++ js/demo.js
@@ -256,25 +256,6 @@ var demo = (function(window, undefined) {
 window.onload = demo.init;
 
 
-
-
-
-
-
-/*$(function() {
-  $('.intro').addClass('go');
-
-  $('.reload').click(function() {
-    $('.intro').removeClass('go').delay(100).queue(function(next) {
-      $('.intro').addClass('go');
-      next();
-    });
-
-  });
-})*/
-
-
 $.noConflict();
-
 
 
